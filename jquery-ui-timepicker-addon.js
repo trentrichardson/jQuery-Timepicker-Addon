44fc32ab449a2a0575070821b0f28d2c3dfa9735
Trent Richardson <trentdrichardson@gmail.com>
2011-01-05 10:00:57 -0500

    Fix regex parsing with separator option

--- jquery-ui-timepicker-addon.js
+++ jquery-ui-timepicker-addon.js
@@ -194,7 +194,7 @@ $.extend(Timepicker.prototype, {
 			// the time should come after x number of characters and a space.
 			// x = at least the length of text specified by the date format
 			var dp_dateFormat = $.datepicker._get(this.inst, 'dateFormat');
-			regstr = '.{' + dp_dateFormat.length + ',}\\s*' + this._defaults.separator + regstr;
+			regstr = '.{' + dp_dateFormat.length + ',}' + this._defaults.separator + regstr;
 		}
 
 		treg = timeString.match(new RegExp(regstr, 'i'));
