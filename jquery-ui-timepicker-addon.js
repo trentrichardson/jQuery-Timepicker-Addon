3649e6a830dbb6b4ddab73966944cdfe679f3ad2
Trent Richardson <trentdrichardson@gmail.com>
2011-05-12 13:36:37 -0400

    Create new dev version

--- jquery-ui-timepicker-addon.js
+++ jquery-ui-timepicker-addon.js
@@ -1,7 +1,7 @@
 /*
 * jQuery timepicker addon
 * By: Trent Richardson [http://trentrichardson.com]
-* Version 0.9.4
+* Version 0.9.5-dev
 * Last Modified: 05/12/2011
 * 
 * Copyright 2011 Trent Richardson
