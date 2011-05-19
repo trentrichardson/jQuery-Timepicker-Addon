c3e1143ce9312146b59c00dd0168297284d565a9
Trent <trsserver@trsserver.(none)>
2011-05-19 19:53:38 -0400

    Added localizations for Dutch and Russian

---
+++ localization/jquery-ui-timepicker-nl.js
@@ -0,0 +1,11 @@
+$.timepicker.regional.nl = {
+	timeOnlyTitle: 'Tijdstip',
+	timeText: 'Tijd',
+	hourText: 'Uur',
+	minuteText: 'Minuut',
+	secondText: 'Seconde',
+	currentText: 'Vandaag',
+	closeText: 'Sluiten',
+	ampm: false
+};
+$.timepicker.setDefaults($.timepicker.regional.nl);
---
+++ localization/jquery-ui-timepicker-ru.js
@@ -0,0 +1,11 @@
+$.timepicker.regional.ru = {
+	timeOnlyTitle: 'Выберите время',
+	timeText: 'Время',
+	hourText: 'Часы',
+	minuteText: 'Минуты',
+	secondText: 'Секунды',
+	currentText: 'Теперь',
+	closeText: 'Закрыть',
+	ampm: false
+};
+$.timepicker.setDefaults($.timepicker.regional.ru);
