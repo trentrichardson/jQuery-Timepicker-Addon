643f20f065634b13b682dfda3111b50b57be6faf
Trent <trsserver@trsserver.(none)>
2011-05-22 08:32:10 -0400

    Updated comments

--- localization/jquery-ui-timepicker-ru.js
+++ localization/jquery-ui-timepicker-ru.js
@@ -1,11 +1,15 @@
-$.timepicker.regional.ru = {
+/* Russian translation for the jQuery Timepicker Addon */
+/* Written by Trent Richardson */
+$.timepicker.regional['ru'] = {
 	timeOnlyTitle: 'Выберите время',
 	timeText: 'Время',
 	hourText: 'Часы',
 	minuteText: 'Минуты',
 	secondText: 'Секунды',
+	timezoneText: 'Время зоны',
 	currentText: 'Теперь',
 	closeText: 'Закрыть',
+	timeFormat: 'hh:mm tt',
 	ampm: false
 };
-$.timepicker.setDefaults($.timepicker.regional.ru);
+$.timepicker.setDefaults($.timepicker.regional['ru']);
