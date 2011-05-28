30f54c8e8aeff429666727d28688682128952efc
Trent <trsserver@trsserver.(none)>
2011-05-28 10:26:42 -0400

    Added Lithuanian localization by Irmantas

---
+++ localization/jquery-ui-timepicker-lt.js
@@ -0,0 +1,16 @@
+/* Lithuanian translation for the jQuery Timepicker Addon */
+/* Written by Irmantas Šiupšinskas */
+$.timepicker.regional['lt'] = {
+	timeOnlyTitle: 'Pasirinkite laiką',
+	timeText: 'Laikas',
+	hourText: 'Valandos',
+	minuteText: 'Minutės',
+	secondText: 'Sekundės',
+	timezoneText: 'Laiko zona',
+	currentText: 'Dabar',
+	closeText: 'Uždaryti',
+	timeFormat: 'hh:mm',
+	ampm: false
+};
+$.timepicker.setDefaults($.timepicker.regional['lt']);
+
