d4187fbde90a13b1e5972cdba4906c29edb24cba
Trent <trsserver@trsserver.(none)>
2011-05-30 21:14:38 -0400

    Added Indonesian localization by nmutiara

---
+++ localization/jquery-ui-timepicker-id.js
@@ -0,0 +1,16 @@
+/* Indonesian translation for the jQuery Timepicker Addon */
+/* Written by Nia */
+$.timepicker.regional['id'] = {
+	timeOnlyTitle: 'Pilih Waktu',
+	timeText: 'Waktu',
+	hourText: 'Pukul',
+	minuteText: 'Menit',
+	secondText: 'Detik',
+	timezoneText: 'Zona Waktu',
+	currentText: 'Sekarang',
+	closeText: 'OK',
+	timeFormat: 'hh:mm tt',
+	ampm: false
+};
+$.timepicker.setDefaults($.timepicker.regional['id']);
+
