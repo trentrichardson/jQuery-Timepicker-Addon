2bea246c03b3aad85003b7b4d4130d10caec2bbd
Marc-Antoine Duhaime <maduhaime@yahoo.com>
2011-06-01 05:55:15 -0700

    Edited localization/jquery-ui-timepicker-fr.js via GitHub

--- localization/jquery-ui-timepicker-fr.js
+++ localization/jquery-ui-timepicker-fr.js
@@ -1,15 +1,17 @@
-/* Czech translation for the jQuery Timepicker Addon */
-/* Written by Thomas Lété */
-$.timepicker.regional['fr'] = {
-	timeOnlyTitle: 'Choisir une heure',
-	timeText: 'Heure',
-	hourText: 'Heures',
-	minuteText: 'Minutes',
-	secondText: 'Secondes',
-	timezoneText: 'Fuseau horaire',
-	currentText: 'Maintenant',
-	closeText: 'Terminé'
-	timeFormat: 'h:m',
-	ampm: false
-};
-$.timepicker.setDefaults($.timepicker.regional['fr']);
+/* French translation for the jQuery Timepicker Addon */
+/* Written by Thomas Lété, corrected by Marc-Antoine Duhaime */
+jQuery(function($){
+	$.timepicker.regional['fr'] = {
+		timeOnlyTitle: 'Choisir une heure',
+		timeText: 'Heure',
+		hourText: 'Heures',
+		minuteText: 'Minutes',
+		secondText: 'Secondes',
+		timezoneText: 'Fuseau horaire',
+		currentText: 'Maintenant',
+		closeText: 'Terminé',
+		timeFormat: 'h:m',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['fr']);
+});
