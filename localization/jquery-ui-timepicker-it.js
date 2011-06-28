9c6c14b3905c5aa4c2054b2b8cc771bb91dc30d7
Marco Del Tongo <info@marcodeltongo.com>
2011-06-29 01:03:06 +0200

    Italian translation

---
+++ localization/jquery-ui-timepicker-it.js
@@ -0,0 +1,17 @@
+/* Italian translation for the jQuery Timepicker Addon */
+/* Written by Marco "logicoder" Del Tongo */
+(function($) {
+    $.timepicker.regional['it'] = {
+        currentText: 'Adesso',
+        closeText: 'Chiudi',
+        ampm: false,
+        timeFormat: 'hh:mm',
+        timeOnlyTitle: 'Scegli orario',
+        timeText: 'Orario',
+        hourText: 'Ora',
+        minuteText: 'Minuto',
+        secondText: 'Secondo',
+        timezoneText: 'Fuso orario'
+    };
+    $.timepicker.setDefaults($.timepicker.regional['it']);
+})(jQuery);
