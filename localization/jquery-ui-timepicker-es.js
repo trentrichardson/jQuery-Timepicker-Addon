bf90861b2a17e750ae6660a3a551bb9b552720c5
Maikel Luis <amok.gentoo@gmail.com>
2011-06-29 17:48:58 -0400

    Add a spanish location file

---
+++ localization/jquery-ui-timepicker-es.js
@@ -0,0 +1,16 @@
+
+$.timepicker.regional['es'] = {
+	timeOnlyTitle: 'Seleccione la hora',
+	timeText: 'Hora',
+	hourText: 'Horas',
+	minuteText: 'Minutos',
+	secondText: 'Segundos',
+	timezoneText: 'Zona horaria',
+	currentText: 'Ahora',
+	closeText: 'Cerrar',
+	timeFormat: 'h:m',
+	ampm: false
+};
+
+$.timepicker.setDefaults($.timepicker.regional['es']);
+
