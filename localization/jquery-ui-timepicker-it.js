13106ee4ee4fd056a2ce271001c0d9ac74b51d2a
Trent Richardson <trentdrichardson@gmail.com>
2011-06-30 17:59:47 -0400

    Added Italian translation by logicoder

---
+++ localization/jquery-ui-timepicker-it.js
@@ -0,0 +1,17 @@
+/* Italian translation for the jQuery Timepicker Addon */
+/* Written by Marco "logicoder" Del Tongo */
+(function($) {
+    $.timepicker.regional['it'] = {
+        timeOnlyTitle: 'Scegli orario',
+        timeText: 'Orario',
+        hourText: 'Ora',
+        minuteText: 'Minuto',
+        secondText: 'Secondo',
+        timezoneText: 'Fuso orario',
+        currentText: 'Adesso',
+        closeText: 'Chiudi',
+        timeFormat: 'hh:mm',
+        ampm: false
+    };
+    $.timepicker.setDefaults($.timepicker.regional['it']);
+})(jQuery);
