05f3db74f246c2c37d6c8c40957e283c495d44b8
Trent Richardson <trentdrichardson@gmail.com>
2011-07-16 15:18:53 -0400

    Added Estonian translation by Karls

---
+++ localization/jquery-ui-timepicker-et.js
@@ -0,0 +1,17 @@
+/* Estonian translation for the jQuery Timepicker Addon */
+/* Written by Karl Sutt (karl@sutt.ee) */
+(function($) {
+	$.timepicker.regional['et'] = {
+		timeOnlyTitle: 'Vali aeg',
+		timeText: 'Aeg',
+		hourText: 'Tund',
+		minuteText: 'Minut',
+		secondText: 'Sekund',
+		timezoneText: 'Ajavöönd',
+		currentText: 'Praegu',
+		closeText: 'Valmis',
+		timeFormat: 'hh:mm tt',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['et']);
+})(jQuery);
