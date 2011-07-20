76544647a51e17a4c4b9e86082b486fc4e1825ee
Trent Richardson <trentdrichardson@gmail.com>
2011-07-20 07:27:44 -0400

    added Greek and Turkish translations

---
+++ localization/jquery-ui-timepicker-el.js
@@ -0,0 +1,17 @@
+/* Hellenic translation for the jQuery Timepicker Addon */
+/* Written by Christos Pontikis */
+(function($) {
+	$.timepicker.regional['el'] = {
+		timeOnlyTitle: 'Επιλογή ώρας',
+		timeText: 'Ώρα',
+		hourText: 'Ώρες',
+		minuteText: 'Λεπτά',
+		secondText: 'Δευτερόλεπτα',
+		timezoneText: 'Ζώνη ώρας',
+		currentText: 'Τώρα',
+		closeText: 'Κλείσιμο',
+		timeFormat: 'hh:mm',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['el']);
+})(jQuery);
---
+++ localization/jquery-ui-timepicker-tr.js
@@ -0,0 +1,17 @@
+/* Turkish translation for the jQuery Timepicker Addon */
+/* Written by Fehmi Can Saglam */
+(function($) {
+	$.timepicker.regional['tr'] = {
+		timeOnlyTitle: 'Zaman Seçiniz',
+		timeText: 'Zaman',
+		hourText: 'Saat',
+		minuteText: 'Dakika',
+		secondText: 'Saniye',
+		timezoneText: 'Zaman Dilimi',
+		currentText: 'Şu an',
+		closeText: 'Tamam',
+		timeFormat: 'hh:mm',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['tr']);
+})(jQuery);
