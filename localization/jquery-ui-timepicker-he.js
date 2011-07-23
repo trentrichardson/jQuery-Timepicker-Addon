083efb98d54c7695f304776b5df069f503afee1a
Trent Richardson <trentdrichardson@gmail.com>
2011-07-23 09:35:00 -0400

    Added Hebrew localization

---
+++ localization/jquery-ui-timepicker-he.js
@@ -0,0 +1,17 @@
+/* Hebrew translation for the jQuery Timepicker Addon */
+/* Written by Lior Lapid */
+(function($) {
+	$.timepicker.regional["he"] = {
+		timeOnlyTitle: "בחרית זמן",
+		timeText: "שעה",
+		hourText: "שעות",
+		minuteText: "דקות",
+		secondText: "שניות",
+		timezoneText: "אזור זמן",
+		currentText: "עכשיו",
+		closeText:"סגור",
+		timeFormat: "hh:mm tt",
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional["he"]);
+})(jQuery);
