9cfffea9d30e5f0faa52030115b9b966d5329c46
Romeo Adrian Cioaba <romeo.cioaba@spotonearth.com>
2011-07-28 01:35:35 +0300

    added romanian localization

---
+++ localization/jquery-ui-timepicker-ro.js
@@ -0,0 +1,17 @@
+/* Romanian translation for the jQuery Timepicker Addon */
+/* Written by Romeo Adrian Cioaba */
+(function($) {
+	$.timepicker.regional['ro'] = {
+		timeOnlyTitle: 'Alegeţi o oră',
+		timeText: 'Timp',
+		hourText: 'Ore',
+		minuteText: 'Minute',
+		secondText: 'Secunde',
+		timezoneText: 'Fus orar',
+		currentText: 'Acum',
+		closeText: 'Închide',
+		timeFormat: 'hh:mm',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['ro']);
+})(jQuery);
