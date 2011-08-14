7eae959879f482f8750902b3d5af3620f48ba430
Michał Pena <cyb.org@cyb.org.pl>
2011-08-14 21:51:28 +0200

    Added Polish translation by Michał Pena

---
+++ localization/jquery-ui-timepicker-pl.js
@@ -0,0 +1,18 @@
+/* German translation for the jQuery Timepicker Addon */
+/* Written by Michał Pena */
+(function($) {
+	$.timepicker.regional['pl'] = {
+		timeOnlyTitle: 'Wybierz godzinę',
+		timeText: 'Czas',
+		hourText: 'Godzina',
+		minuteText: 'Minuta',
+		secondText: 'Sekunda',
+		millisecText: 'Milisekunda',
+		timezoneText: 'Strefa czasowa',
+		currentText: 'Teraz',
+		closeText: 'Gotowe',
+		timeFormat: 'hh:mm tt',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['pl']);
+})(jQuery);
