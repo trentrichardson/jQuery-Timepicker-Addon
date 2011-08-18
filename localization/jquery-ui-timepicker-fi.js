2cbd5a7316739266bff0b35e7269e1aa69a6dd11
Juga Paazmaya <olavic@gmail.com>
2011-08-18 18:31:54 +0300

    Finnish translation

---
+++ localization/jquery-ui-timepicker-fi.js
@@ -0,0 +1,17 @@
+/* Finnish translation for the jQuery Timepicker Addon */
+/* Written by Juga Paazmaya (http://github.com/paazmaya) */
+(function($) {
+    $.timepicker.regional['fi'] = {
+        timeOnlyTitle: 'Valitse aika',
+        timeText: 'Aika',
+        hourText: 'Tunti',
+        minuteText: 'Minuutti',
+        secondText: 'Sekunti',
+        timezoneText: 'Aikavyöhyke',
+        currentText: 'Nyt',
+        closeText: 'Sulje',
+        timeFormat: 'hh:mm',
+        ampm: false
+    };
+    $.timepicker.setDefaults($.timepicker.regional['fi']);
+})(jQuery);
