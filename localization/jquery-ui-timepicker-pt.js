9abf0e8a8aa2e429a2c704feb12c3bced15247be
Luan Almeida <luanlmd@gmail.com>
2011-08-19 15:41:51 -0300

    Portuguese translation

---
+++ localization/jquery-ui-timepicker-pt.js
@@ -0,0 +1,17 @@
+/* Spanish translation for the jQuery Timepicker Addon */
+/* Written by Luan Almeida */
+(function($) {
+	$.timepicker.regional['pt'] = {
+		timeOnlyTitle: 'Escolha uma hora',
+		timeText: 'Hora',
+		hourText: 'Horas',
+		minuteText: 'Minutos',
+		secondText: 'Segundos',
+		timezoneText: 'Fuso horário',
+		currentText: 'Agora',
+		closeText: 'Fechar',
+		timeFormat: 'hh:mm',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['pt']);
+})(jQuery);
