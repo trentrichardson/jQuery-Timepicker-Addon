04a37a69a486aec1a24cc0e79ac61305cd8bffaf
David Vallner <david@vallner.net>
2011-09-30 00:44:58 +0200

    Slovak localisation
    Made by modifying the existing Czech localisation.

---
+++ localization/jquery-ui-timepicker-sk.js
@@ -0,0 +1,18 @@
+/* Slovak translation for the jQuery Timepicker Addon */
+/* Czech translation originally by Ondřej Vodáček */
+/* Modified for Slovak by David Vallner */
+(function ($) {
+	$.timepicker.regional['sk'] = {
+		timeOnlyTitle: 'Zvoľte čas',
+		timeText: 'Čas',
+		hourText: 'Hodiny',
+		minuteText: 'Minúty',
+		secondText: 'Sekundy',
+		timezoneText: 'Časové pásmo',
+		currentText: 'Teraz',
+		closeText: 'Zavrieť',
+		timeFormat: 'h:m',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['sk']);
+})(jQuery);
