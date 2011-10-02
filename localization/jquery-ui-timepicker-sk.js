7d63691052bc8b04fdb7a90940d74defa5518853
Trent Richardson <trentdrichardson@gmail.com>
2011-10-02 09:15:41 -0400

    Added Slovak translation

---
+++ localization/jquery-ui-timepicker-sk.js
@@ -0,0 +1,20 @@
+/* Slovak translation for the jQuery Timepicker Addon */
+/* Written by David Vallner */
+(function($) {
+	$.timepicker.regional['sk'] = {
+		timeOnlyTitle: 'Zvoľte čas',
+		timeText: 'Čas',
+		hourText: 'Hodiny',
+		minuteText: 'Minuty',
+		secondText: 'Sekundy',
+		millisecText: 'Milisekundy',
+		timezoneText: 'Časové pásmo',
+		currentText: 'Teraz',
+		closeText: 'Zavřít',
+		timeFormat: 'h:m',
+		amNames: ['dop.', 'AM', 'A'],
+		pmNames: ['odp.', 'PM', 'P'],
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['sk']);
+})(jQuery);
