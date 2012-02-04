392b51f4df1fea1c5c9d4597975a1353f55afb17
JoongSeob Vito Kim <dorajissanai@nate.com>
2012-02-04 14:35:16 +0000

    Added Korean Locale

---
+++ localization/jquery-ui-timepicker-ko.js
@@ -0,0 +1,20 @@
+/* Korean translation for the jQuery Timepicker Addon */
+/* Written by JoongSeob Vito Kim (http://www.dorajistyle.pe.kr) */
+(function($) {
+	$.timepicker.regional['ko'] = {
+		timeOnlyTitle: '시간을 선택하세요.',
+		timeText: '시간',
+		hourText: '시',
+		minuteText: '분',
+		secondText: '초',
+		millisecText: '밀리초',
+		timezoneText: '시각대',
+		currentText: '현재',
+		closeText: '닫기',
+		timeFormat: 'hh:mm',
+		amNames: ['오전', 'AM'],
+		pmNames: ['오후', 'PM'],
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['ko']);
+})(jQuery);
