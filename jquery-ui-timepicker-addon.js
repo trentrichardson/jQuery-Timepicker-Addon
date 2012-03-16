704c0b9d33d4f8a675f3802a63fddcb3f9186f90
Trent Richardson <trentdrichardson@gmail.com>
2012-03-16 14:12:31 -0400

    fix formatting

--- jquery-ui-timepicker-addon.js
+++ jquery-ui-timepicker-addon.js
@@ -235,27 +235,27 @@ $.extend(Timepicker.prototype, {
 	_parseTime: function(timeString, withDate) {
 		if (!this.inst) 
 			this.inst = $.datepicker._getInst(this.$input[0]);
-        
-        if (withDate || !this._defaults.timeOnly) 
-        {
+		
+		if (withDate || !this._defaults.timeOnly) 
+		{
 			var dp_dateFormat = $.datepicker._get(this.inst, 'dateFormat');
-            try {
-                var parseRes = parseDateTimeInternal(dp_dateFormat, this._defaults.timeFormat, timeString, $.datepicker._getFormatConfig(this.inst), this._defaults);
-                if (!parseRes.timeObj) return false;
-                $.extend(this, parseRes.timeObj);
-            } catch (err)
-            {
-                return false;
-            }
-            return true;
-        }
-        else
-        {
-            var timeObj = $.datepicker.parseTime(this._defaults.timeFormat, timeString, this._defaults);
-            if(!timeObj) return false;
-            $.extend(this, timeObj);
-            return true;
-        }
+			try {
+				var parseRes = parseDateTimeInternal(dp_dateFormat, this._defaults.timeFormat, timeString, $.datepicker._getFormatConfig(this.inst), this._defaults);
+				if (!parseRes.timeObj) return false;
+				$.extend(this, parseRes.timeObj);
+			} catch (err)
+			{
+				return false;
+			}
+			return true;
+		}
+		else
+		{
+			var timeObj = $.datepicker.parseTime(this._defaults.timeFormat, timeString, this._defaults);
+			if(!timeObj) return false;
+			$.extend(this, timeObj);
+			return true;
+		}
 	},
 	
 	//########################################################################
