d6cc2ce254329145a3896417dfa20340f43c7a75
Alexander Pepper <alexander.pepper@infopark.de>
2012-03-26 14:31:04 +0200

    Refactored internals to calculate the timeZoneString.

--- jquery-ui-timepicker-addon.js
+++ jquery-ui-timepicker-addon.js
@@ -1423,15 +1423,20 @@ var selectLocalTimeZone = function(tp_inst, date)
 	if (tp_inst && tp_inst._defaults.showTimezone && tp_inst.timezone_select) {
 		tp_inst.useLocalTimezone = true;
 		var now = typeof date !== 'undefined' ? date : new Date();
-		var tzoffset = now.getTimezoneOffset(); // If +0100, returns -60
-		var tzsign = tzoffset > 0 ? '-' : '+';
-		tzoffset = Math.abs(tzoffset);
-		var tzmin = tzoffset % 60;
-		tzoffset = tzsign + ('0' + (tzoffset - tzmin) / 60).slice(-2) + ('0' + tzmin).slice(-2);
+		var tzoffset = timeZoneString(now);
 		if (tp_inst._defaults.timezoneIso8601)
 			tzoffset = tzoffset.substring(0, 3) + ':' + tzoffset.substring(3);
 		tp_inst.timezone_select.val(tzoffset);
 	}
+}
+
+// Input: Date Object
+// Output: String with timezone offset, e.g. '+0100'
+var timeZoneString = function(date)
+{
+	var off = date.getTimezoneOffset() * -10100 / 60;
+	var timezone = (off >= 0 ? '+' : '-') + Math.abs(off).toString().substr(1);
+	return timezone;
 }
 
 $.timepicker = new Timepicker(); // singleton instance
