85dc6175bc2236e2ae0106323b6f380542093a4e
Evandro Araújo <evandroprogram@gmail.com>
2012-07-17 14:00:34 -0300

    Replaced 'Hora' by 'Horário', make more sense in portuguese language.

--- localization/jquery-ui-timepicker-pt-BR.js
+++ localization/jquery-ui-timepicker-pt-BR.js
@@ -1,14 +1,15 @@
 /* Brazilian Portuguese translation for the jQuery Timepicker Addon */
 /* Written by Diogo Damiani (diogodamiani@gmail.com) */
+/* Revised and corrected by Evandro Ara√∫jo (evandroprogram@gmail.com) v0.1 */
 (function ($) {
 	$.timepicker.regional['pt-BR'] = {
-		timeOnlyTitle: 'Escolha a hora',
-		timeText: 'Hora',
-		hourText: 'Horas',
+		timeOnlyTitle: 'Escolha o hor&aacute;rio',
+		timeText: 'Hor&aacute;rio',
+		hourText: 'Hora',
 		minuteText: 'Minutos',
 		secondText: 'Segundos',
 		millisecText: 'Milissegundos',
-		timezoneText: 'Fuso hor·rio',
+		timezoneText: 'Fuso hor&aacute;rio',
 		currentText: 'Agora',
 		closeText: 'Fechar',
 		timeFormat: 'hh:mm',
