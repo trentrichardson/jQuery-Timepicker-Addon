d31f500103ce562a03524329710ed16c94ac02e3
Trent Richardson <trentdrichardson@gmail.com>
2012-10-20 14:16:56 -0400

    Update last modified date

--- jquery-ui-sliderAccess.js
+++ jquery-ui-sliderAccess.js
@@ -1,8 +1,8 @@
 /*
  * jQuery UI Slider Access
  * By: Trent Richardson [http://trentrichardson.com]
- * Version 0.2
- * Last Modified: 12/02/2011
+ * Version 0.3
+ * Last Modified: 10/20/2012
  * 
  * Copyright 2011 Trent Richardson
  * Dual licensed under the MIT and GPL licenses.
