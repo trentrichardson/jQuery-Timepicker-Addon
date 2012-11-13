a034b16ece9489a3f8defc0557cb40bb39e3c728
vladutcornel <cornel@scoalaweb.com>
2012-11-13 15:15:17 +0100

    Added weekdaz names and month names

--- localization/jquery-ui-timepicker-ro.js
+++ localization/jquery-ui-timepicker-ro.js
@@ -1,20 +1,33 @@
-/* Romanian translation for the jQuery Timepicker Addon */
-/* Written by Romeo Adrian Cioaba */
+/* German translation for the jQuery Timepicker Addon */
+/* Written by Marvin */
+/* Updated by vladutcornel
+ * * Added week names
+ * * Added month names
+ * * Set the first day to Monday
+ */
 (function($) {
-	$.timepicker.regional['ro'] = {
-		timeOnlyTitle: 'Alegeţi o oră',
-		timeText: 'Timp',
-		hourText: 'Ore',
-		minuteText: 'Minute',
-		secondText: 'Secunde',
-		millisecText: 'Milisecunde',
-		timezoneText: 'Fus orar',
-		currentText: 'Acum',
-		closeText: 'Închide',
-		timeFormat: 'HH:mm',
-		amNames: ['AM', 'A'],
-		pmNames: ['PM', 'P'],
-		isRTL: false
-	};
-	$.timepicker.setDefaults($.timepicker.regional['ro']);
+        $.timepicker.regional['de'] = {
+                timeOnlyTitle: 'Zeit Wählen',
+                timeText: 'Zeit',
+                hourText: 'Stunde',
+                minuteText: 'Minute',
+                secondText: 'Sekunde',
+                millisecText: 'Millisekunde',
+                timezoneText: 'Zeitzone',
+                currentText: 'Jetzt',
+                closeText: 'Fertig',
+                timeFormat: 'HH:mm',
+                amNames: ['vorm.', 'AM', 'A'],
+                pmNames: ['nachm.', 'PM', 'P'],
+                isRTL: false,
+                firstDay: 1,
+                monthNames: ['Ianuarie','Februarie','Martie','Aprilie','Mai','Iunie',
+                'Iulie','August','Septembrie','Octombrie','Noembrie','Decembrie'],
+                monthNamesShort: ['Ian','Feb','Mar','Apr','Mai','Iun',
+                'Iul','Aug','Sep','Oct','Nov','Dec'],
+                dayNames: ['Duminică', 'Luni' , 'Marţi','Miercuri','Joi','Vineri','Sâmbătă'],
+                dayNamesShort: ['Dum', 'Lun' , 'Mar','Mie','Joi','Vin','Sâm'],
+                dayNamesMin: ['D', 'L' , 'Ma','Mi','J','V','S']
+        };
+        $.timepicker.setDefaults($.timepicker.regional['de']);
 })(jQuery);
