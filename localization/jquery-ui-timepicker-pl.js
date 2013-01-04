81869747def793a8d907d8c52e13c949484d4d72
lukaszkoziara <siarakoza@gmail.com>
2013-01-04 13:41:50 +0100

    Update localization/jquery-ui-timepicker-pl.js
    My proposition to update polish localization file according to Marvin german localization find on http://trentrichardson.com/2011/05/20/timepicker-localizations-on-github/

--- localization/jquery-ui-timepicker-pl.js
+++ localization/jquery-ui-timepicker-pl.js
@@ -1,7 +1,16 @@
 /* Polish translation for the jQuery Timepicker Addon */
 /* Written by Michał Pena */
+/* Updated by Łukasz Koziara */
 (function($) {
 	$.timepicker.regional['pl'] = {
+		monthNames: ['Styczeń','Luty','Marzec','Kwiecień','Maj','Czerwiec',
+		'Lipiec','Sierpień','Wrzesień','Październik','Listopad','Grudzień'],
+		monthNamesShort: ['Sty','Lut','Mar','Kwi','Maj','Cze',
+		'Lip','Sie','Wrz','Paź','Lis','Gru'],
+		dayNames: ['Niedziela','Poniedziałek','Wtorek','Środa','Czwartek','Piątek','Sobota'],
+		dayNamesShort: ['Ndz','Pon','Wt','Śr','Czw','Pt','Sob'],
+		dayNamesMin: ['Nd','Pn','Wt','Śr','Cz','Pt','Sb'],
+		weekHeader: 'Tydz',
 		timeOnlyTitle: 'Wybierz godzinę',
 		timeText: 'Czas',
 		hourText: 'Godzina',
