948ffad8ab8a098c2d8b5e022c636073da5f324b
msajko <msajko@gmail.com>
2013-03-07 15:15:36 +0100

    Croatian translation

---
+++ localization/jquery-ui-timepicker-hr.js
@@ -0,0 +1,20 @@
+/* Croatian translation for the jQuery Timepicker Addon */
+/* Written by Mladen */
+(function($) {
+	$.timepicker.regional['hr'] = {
+		timeOnlyTitle: 'Odaberi vrijeme',
+		timeText: 'Vrijeme',
+		hourText: 'Sati',
+		minuteText: 'Minute',
+		secondText: 'Sekunde',
+		millisecText: 'Milisekunde',
+		timezoneText: 'Vremenska zona',
+		currentText: 'Sada',
+		closeText: 'Gotovo',
+		timeFormat: 'HH:mm',
+		amNames: ['a.m.', 'AM', 'A'],
+		pmNames: ['p.m.', 'PM', 'P'],
+		isRTL: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['hr']);
+})(jQuery);
