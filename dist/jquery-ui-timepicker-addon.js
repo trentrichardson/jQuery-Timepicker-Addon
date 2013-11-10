fe684fac3d3de25b31950d3cdf7c72c6c5e59fce
Trent Richardson <trentdrichardson@gmail.com>
2013-11-10 15:35:37 -0500

    New version

--- dist/jquery-ui-timepicker-addon.js
+++ dist/jquery-ui-timepicker-addon.js
@@ -1,4 +1,4 @@
-/*! jQuery Timepicker Addon - v1.4.1 - 2013-10-23
+/*! jQuery Timepicker Addon - v1.4.1 - 2013-11-10
 * http://trentrichardson.com/examples/timepicker
 * Copyright (c) 2013 Trent Richardson; Licensed MIT */
 (function ($) {
