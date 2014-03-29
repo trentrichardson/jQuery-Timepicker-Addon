3d0ada0bd9613864da5af718b6702dc435a24b4b
Trent Richardson <trentdrichardson@gmail.com>
2014-03-29 14:45:51 -0400

    Ready to merge

--- dist/jquery-ui-timepicker-addon.js
+++ dist/jquery-ui-timepicker-addon.js
@@ -1,4 +1,4 @@
-/*! jQuery Timepicker Addon - v1.4.4 - 2013-11-30
+/*! jQuery Timepicker Addon - v1.4.4 - 2014-03-29
 * http://trentrichardson.com/examples/timepicker
 * Copyright (c) 2014 Trent Richardson; Licensed MIT */
 (function ($) {
