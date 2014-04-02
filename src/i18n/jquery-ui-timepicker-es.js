bfa0c300f5d8168e5a224962c3a8f7fb7da19d2e
Milton Mazzarri <milmazz@gmail.com>
2014-04-02 10:18:07 -0600

    Added new spanish translations.

--- src/i18n/jquery-ui-timepicker-es.js
+++ src/i18n/jquery-ui-timepicker-es.js
@@ -1,21 +1,32 @@
 /* Spanish translation for the jQuery Timepicker Addon */
 /* Written by Ianaré Sévi */
 (function($) {
-	$.timepicker.regional['es'] = {
-		timeOnlyTitle: 'Elegir una hora',
-		timeText: 'Hora',
-		hourText: 'Horas',
-		minuteText: 'Minutos',
-		secondText: 'Segundos',
-		millisecText: 'Milisegundos',
-		microsecText: 'Microsegundos',
-		timezoneText: 'Huso horario',
-		currentText: 'Ahora',
-		closeText: 'Cerrar',
-		timeFormat: 'HH:mm',
-		amNames: ['a.m.', 'AM', 'A'],
-		pmNames: ['p.m.', 'PM', 'P'],
-		isRTL: false
-	};
-	$.timepicker.setDefaults($.timepicker.regional['es']);
+    $.timepicker.regional['es'] = {
+        amNames: ['a.m.', 'AM', 'A'],
+        closeText: 'Cerrar',
+        currentText: 'Ahora',
+        dateFormat: 'dd/mm/yy',
+        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+        dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+        dayNamesShort: ['Dom', 'Lun', 'Mart', 'Miérc', 'Juev', 'Vier', 'Sáb'],
+        firstDay: 1,
+        hourText: 'Horas',
+        isRTL: false,
+        microsecText: 'Microsegundos',
+        millisecText: 'Milisegundos',
+        minuteText: 'Minutos',
+        monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Oct', 'Nov', 'Dic'],
+        nextText: 'Siguiente',
+        pmNames: ['p.m.', 'PM', 'P'],
+        prevText: 'Previo',
+        secondText: 'Segundos',
+        showMonthAfterYear: false,
+        timeFormat: 'HH:mm',
+        timeOnlyTitle: 'Elegir una hora',
+        timeText: 'Hora',
+        timezoneText: 'Huso horario',
+        yearSuffix: ''
+    };
+    $.timepicker.setDefaults($.timepicker.regional['es']);
 })(jQuery);
