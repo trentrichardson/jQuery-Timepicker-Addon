d0ff7511623296a245e79fec83634f22ae399003
carlitoxenlaweb <carloscemg@gmail.com>
2014-07-07 09:56:49 -0430

    Update jquery-ui-timepicker-es.js
    Added:
    - Months
    - Days
    - Date Format
    - Shorts Names
    
    Fixed:
    - Editorial Errors
    
    PD: Thanks for this plugin/addon and its translation

--- src/i18n/jquery-ui-timepicker-es.js
+++ src/i18n/jquery-ui-timepicker-es.js
@@ -1,7 +1,14 @@
 /* Spanish translation for the jQuery Timepicker Addon */
 /* Written by Ianaré Sévi */
+/* Modified by Carlos Martínez */
 (function($) {
 	$.timepicker.regional['es'] = {
+		monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+		monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'],
+		dayNames: ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'],
+		dayNamesShort: ['Dom','Lun','Mar','Mie','Jue','Vie','Sab'],
+		dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sa'],
+		weekHeader: 'Sem',
 		timeOnlyTitle: 'Elegir una hora',
 		timeText: 'Hora',
 		hourText: 'Horas',
@@ -9,12 +16,13 @@
 		secondText: 'Segundos',
 		millisecText: 'Milisegundos',
 		microsecText: 'Microsegundos',
-		timezoneText: 'Huso horario',
-		currentText: 'Ahora',
+		timezoneText: 'Uso horario',
+		currentText: 'Hoy',
 		closeText: 'Cerrar',
 		timeFormat: 'HH:mm',
 		amNames: ['a.m.', 'AM', 'A'],
 		pmNames: ['p.m.', 'PM', 'P'],
+		dateFormat: 'dd/mm/yy',
 		isRTL: false
 	};
 	$.timepicker.setDefaults($.timepicker.regional['es']);
