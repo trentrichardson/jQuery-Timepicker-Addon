4db155d3ca89b5d6c05f6efad11b71b15eedd908
bogatykh <bogatykh@gmail.com>
2014-11-24 10:28:44 +0200

    Create jquery-ui-timepicker-lv.js
    Latvian localization added.

---
+++ src/i18n/https/github.com/trentrichardson/jquery-ui-timepicker-lv.js
@@ -0,0 +1,22 @@
+/* Latvian translation for the jQuery Timepicker Addon */
+/* Written by Dmitry Bogatykh */
+(function($) {
+	$.timepicker.regional['lv'] = {
+		timeOnlyTitle: 'Ievadiet laiku',
+		timeText: 'Laiks',
+		hourText: 'Stundas',
+		minuteText: 'Minūtes',
+		secondText: 'Sekundes',
+		millisecText: 'Milisekundes',
+		microsecText: 'Mikrosekundes',
+		timezoneText: 'Laika josla',
+		currentText: 'Tagad',
+		closeText: 'Aizvert',
+		timeFormat: 'HH:mm',
+		amNames: ['AM', 'AM', 'A'],
+		pmNames: ['PM', 'PM', 'P'],
+		isRTL: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['lv']);
+})(jQuery);
+
