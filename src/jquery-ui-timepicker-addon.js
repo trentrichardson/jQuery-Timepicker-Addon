e9338ef96035cbcf3742a04b7c6613758552934c
Pletenev Stanislav Viktorovich <pletenev-sv@fxclub.org>
2015-02-02 18:50:10 +0600

    Added option afterInject which receives function, and triggered after timepicker injection complete

--- src/jquery-ui-timepicker-addon.js
+++ src/jquery-ui-timepicker-addon.js
@@ -109,7 +109,8 @@
 			controlType: 'slider',
 			oneLine: false,
 			defaultValue: null,
-			parse: 'strict'
+			parse: 'strict',
+			afterInject: null
 		};
 		$.extend(this._defaults, this.regional['']);
 	};
@@ -319,6 +320,7 @@
 			this.timeDefined = this._parseTime(currDT);
 			this._limitMinMaxDateTime(dp_inst, false);
 			this._injectTimePicker();
+			this._afterInject();
 		},
 
 		/*
@@ -352,6 +354,13 @@
 				}
 				$.extend(this, timeObj);
 				return true;
+			}
+		},
+
+		_afterInject: function() {
+			var o = this.inst.settings;
+			if ($.isFunction(o.afterInject)) {
+				o.afterInject.call(this);
 			}
 		},
 
