2a8781b8e622ef7d13988cfb14de52b6cc82dd2b
simbulu <torkil.zachariassen@gmail.com>
2017-02-03 21:46:34 +0000

    Create jquery-ui-timepicker-fo.js
    Faroese translation for the jQuery Timepicker Addon

---
+++ dist/i18n/jquery-ui-timepicker-fo.js
@@ -0,0 +1,23 @@
+/* Faroese translation for the jQuery Timepicker Addon */
+/* Written by Torkil Zachariassen */
+/* File: jquery-ui-timepicker-fo.js */
+(function ($) {
+    $.timepicker.regional['fo'] = {
+        timeOnlyTitle: 'Vel tíð',
+        timeText: 'Tíð',
+        hourText: 'Tími',
+        minuteText: 'Minuttur',
+        secondText: 'Sekund',
+        millisecText: 'Millisekund',
+        microsecText: 'Mikrosekund',
+        timezoneText: 'Tíðarsona',
+        currentText: 'Nú',
+        closeText: 'Lat aftur',
+        timeFormat: 'HH:mm',
+        timeSuffix: '',
+        amNames: ['am', 'AM', 'A'],
+        pmNames: ['pm', 'PM', 'P'],
+        isRTL: false
+    };
+    $.timepicker.setDefaults($.timepicker.regional['fo']);
+})(jQuery);
