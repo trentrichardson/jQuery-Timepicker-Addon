c7c9e27a282960b9c6e77915e5f423e8d615fdbf
AnasTHH <anas.naim@hotmail.com>
2018-09-17 10:43:51 +0400

    jquery-ui-timepicker-ar.js

---
+++ dist/i18n/jquery-ui-timepicker-ar.js
@@ -0,0 +1,22 @@
+/* Arabic translation for the jQuery Timepicker Addon */
+/* Written by Anas Naim */
+(function($) {
+    $.timepicker.regional['fa'] = {
+        timeOnlyTitle: 'اختر الوقت',
+        timeText: 'وقت',
+        hourText: 'ساعة',
+        minuteText: 'دقیقه',
+        secondText: 'ثانیه',
+        millisecText: 'میلی ثانیه',
+        microsecText: 'میکرو ثانیه',
+        timezoneText: 'منطقة زمنية',
+        currentText: 'الان',
+        closeText: 'اغلاق',
+        timeFormat: 'HH:mm',
+        timeSuffix: '',
+        amNames: ['صباحا', 'AM', 'A'],
+        pmNames: ['مساء', 'PM', 'P'],
+        isRTL: true
+    };
+    $.timepicker.setDefaults($.timepicker.regional['ar']);
+})(jQuery);
