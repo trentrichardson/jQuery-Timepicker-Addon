92e40a35976ea0de435afc8d73303980f45c63d4
Hilary Jendrasiak <sylogista@sylogista.pl>
2020-07-27 21:15:40 +0200

    Fix i18n pl

--- src/i18n/jquery-ui-timepicker-pl.js
+++ src/i18n/jquery-ui-timepicker-pl.js
@@ -1,5 +1,6 @@
 /* Polish translation for the jQuery Timepicker Addon */
 /* Written by Michał Pena */
+/* Written by Hilary Jendrasiak <sylogista@sylogista.pl> */
 (function($) {
 	$.timepicker.regional['pl'] = {
 		timeOnlyTitle: 'Wybierz godzinę',
@@ -13,9 +14,15 @@
 		currentText: 'Teraz',
 		closeText: 'Gotowe',
 		timeFormat: 'HH:mm',
+		dateFormat: 'dd.mm.yy',
 		timeSuffix: '',
 		amNames: ['AM', 'A'],
 		pmNames: ['PM', 'P'],
+		monthNames: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
+		monthNamesShort: ['Sty', 'Lut', 'Mar', 'Kwi', 'Maj', 'Cze', 'Lip', 'Sie', 'Wrz', 'Paź', 'Lis', 'Gru'],
+		dayNames: ['niedziela', 'poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota'],
+		dayNamesMin: ['ndz', 'pon', 'wt', 'śr', 'czw', 'pt', 'sob', ],
+		firstDay: 1,
 		isRTL: false
 	};
 	$.timepicker.setDefaults($.timepicker.regional['pl']);
